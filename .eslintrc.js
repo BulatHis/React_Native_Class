809b81df5d913d95cdcc29e8f3459dc871fad648
Bulat <bulatxisameev@gmail.com>
2024-09-29 18:38:17 +0300

    Task1

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+{
+    "extends": "eslint:recommended",
+    "parser": "babel-eslint",
+    "env": {
+      "es6": true,
+      "node": true,
+      "browser": true
+    },
+    "rules": {
+      "no-console": "off"
+    }
+  }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,82 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { View, ScrollView, Text, Image, StyleSheet } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+export default function App() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <ScrollView style={styles.container}>
+      <View style={styles.simpleView}>
+        <Text style={styles.text}>Простой View с текстом</Text>
+      </View>
+
+      <View style={styles.imageView}>
+        <Text style={styles.text}> изображение</Text>
+        <Image source={ require ( '@/assets/images/coal-price.jpg') } />
+      </View>
+
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.text}>
+           текст в ScrollView
+        </Text>
+        <Text style={styles.text}>
+          Ещё сверху текст
+        </Text>
+        <Text style={styles.text}>
+          Доп текст для скролла
+        </Text>
+        <Text style={styles.text}>
+          Ещё сверху текст
+        </Text>
+        <Text style={styles.text}>
+          Ещё сверху текст
+        </Text>
+        <Text style={styles.text}>
+          Ещё сверху текст
+        </Text>
+        <Text style={styles.text}>
+          Ещё сверху текст
+        </Text>
+        <Text style={styles.text}>
+          Ещё сверху текст
+        </Text>
+        <Text style={styles.text}>
+          Ещё сверху текст
+        </Text>
+        <Text style={styles.text}>
+          Ещё сверху текст
+        </Text>
+        
+      </ScrollView>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  simpleView: {
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+    marginBottom: 20,
+  },
+  imageView: {
+    padding: 20,
+    backgroundColor: '#e0f7fa',
+    marginBottom: 20,
     alignItems: 'center',
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  scrollView: {
+    padding: 20,
+    backgroundColor: '#ffecb3',
+    height: 200,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  image: {
+    width: 100,
+    height: 100,
+    marginTop: 10,
+  },
+  text: {
+    fontSize: 16,
   },
 });
