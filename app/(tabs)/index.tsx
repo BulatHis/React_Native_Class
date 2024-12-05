fc542953bcb4f9a1a90a5afaac32c338521f7e56
Bulat <bulatxisameev@gmail.com>
2024-12-05 21:20:40 +0300

    Task9

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -20,12 +20,8 @@ const App = () => {
 
   return (
     <NavigationContainer linking={DeepLinking.linking} independent={true}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="home" component={AppNavigator} />
-        <Stack.Screen name="INIT" component={InitScreen} />
-        <Stack.Screen name="AUTH_MAIN" component={AuthMainScreen} />
-        {/* Другие экраны */}
-      </Stack.Navigator>
+    <AppNavigator/>
+     
     </NavigationContainer>
   );
 };
